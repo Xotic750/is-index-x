25a508a2e5b85b8d04342055c6525b6396e7663a
Graham Fairweather <xotic750@gmail.com>
2019-07-22 00:56:38 +0200

    :bookmark: v2.0.9
    Auto build

--- dist/is-index-x.js
+++ dist/is-index-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-19T22:07:18.157Z",
+  "date": "2019-07-21T22:56:32.595Z",
   "describe": "",
   "description": "Determine whether the passed value is a zero based index.",
   "file": "is-index-x.js",
   "hash": "fee9a9efbdda130ad0b0",
   "license": "MIT",
-  "version": "2.0.8"
+  "version": "2.0.9"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
