65bb403b9393564ee014aea6f83b6621d91e28e1
Graham Fairweather <xotic750@gmail.com>
2019-07-31 19:41:44 +0200

    Package rework

--- dist/is-index-x.js
+++ dist/is-index-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-31T16:12:38.847Z",
+  "date": "2019-07-31T17:41:38.782Z",
   "describe": "",
   "description": "Determine whether the passed value is a zero based index.",
   "file": "is-index-x.js",
